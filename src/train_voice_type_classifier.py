6916d368d69d7e667d7c416ad777e43d027d67d8
NeleSuffo <nele.suffo@leuphana.de>
2025-05-07 22:21:01 +0200

    feat: implement optimized pipeline for multi-label segmentation and evaluation

--- src/train_voice_type_classifier.py
+++ src/train_voice_type_classifier.py
@@ -1,8 +1,10 @@
 import torch
 from pyannote.database import registry, FileFinder
 from pyannote.audio.tasks import MultiLabelSegmentation
+from pyannote.audio.pipelines import MultiLabelSegmentation as MultiLabelSegmentationPipeline
 from voice_type_classifier import VoiceTypeClassifier
 import pytorch_lightning as pl
+from pyannote.pipeline import Optimizer
 
 # Initialize file finder
 file_finder = FileFinder()
@@ -14,9 +16,10 @@ protocol = registry.get_protocol('ChildLens.SpeakerDiarization.audio', preproces
 # Define the MultiLabelSegmentation task
 mls_task = MultiLabelSegmentation(
     protocol,
+    cache="/home/nele_pauline_suffo/ProcessedData/vtc_childlens/cache/task_cache.npz",
     duration=2.0,
     batch_size=32,
-    num_workers=0,  # Avoid multi-process issues on CPU
+    num_workers=47,
     classes=['kchi', 'och', 'mal', 'fem', 'ovh']
 )
 
@@ -36,3 +39,52 @@ trainer = pl.Trainer(
     default_root_dir="models/"
 )
 trainer.fit(mls_model)
+
+# define the pipeline for multi-label segmentation
+pipeline = MultiLabelSegmentationPipeline(segmentation=mls_model, share_min_duration=True, fscore=True)
+
+# Define the initial parameters for the pipeline
+initial_params = {
+    "thresholds": {
+        "kchi": {"onset": 0.6, "offset": 0.4},
+        "och": {"onset": 0.6, "offset": 0.4},
+        "fem": {"onset": 0.6, "offset": 0.4},
+        "mal": {"onset": 0.6, "offset": 0.4},
+        "ovh": {"onset": 0.6, "offset": 0.4},
+    },
+    "min_duration_on": 0.0,
+    "min_duration_off": 0.0,
+}
+
+# Instantiate the pipeline with the parameters
+pipeline.instantiate(initial_params)
+
+# Freeze the pipeline with the specified parameters
+pipeline.freeze({'min_duration_on': 0.0, 'min_duration_off': 0.0})
+
+optimizer = Optimizer(pipeline)
+optimizer.tune(list(protocol.development()),
+               warm_start=initial_params,
+               n_iterations=20,
+               show_progress=False)
+
+optimized_params = optimizer.best_params
+
+# print and save the optimized parameters
+for key, value in optimized_params.items():
+    print(f"{key}: {value}")
+with open("optimized_parameters.txt", "w") as f:
+    for key, value in optimized_params.items():
+        f.write(f"{key}: {value}\n")
+        
+optimized_pipeline = pipeline.instantiate(optimized_params)
+
+# Evaluate the optimized pipeline
+metric = DetectionErrorRate()
+
+for file in protocol.test():
+    speech = optimized_pipeline(file)
+    _ = metric(file['annotation'], speech, uem=file['annotated'])
+
+detection_error_rate = abs(metric)
+print(f'Detection error rate = {detection_error_rate * 100:.1f}%')
